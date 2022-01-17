9973573d8f23de6015a729dcaca3e190dd1faf97
Mioszek <228871@edu.p.lodz.pl>
2022-01-17 10:32:37 +0100

    1.5.1 extracted main method in RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,27 +7,31 @@ class RhymersDemo {
 	public static final int BOUND = 20;
 	public static void main(String[] args) {
 		final RhymersFactory factory = new DefaultRhymersFactory();
-		
+
+		extracted(factory);
+
+	}
+
+	private static void extracted(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-		
+
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(BOUND));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
 	}
-	
+
 }
